89031de268b1fba2aff627198d9776c5b8fdb238
capetdevalois <2703998852@qq.com>
2024-08-07 23:37:29 +0800

    Site updated: 2024-08-07 23:37:23

---
+++ dist/music.js
@@ -0,0 +1,17 @@
+const ap = new APlayer({
+    container: document.getElementById("aplayer"),
+    fixed: true,
+    autoplay: true,
+    theme: "#000",
+    mutex: true,
+    lrcType: 0,
+    audio: [
+        {
+            name: "幻想郷 ～ Lotus Land Story",
+            artist: "上海アリス幻樂団",
+            url: "http://music.163.com/song/media/outer/url?id=510025.mp3",
+            cover:
+                "https://p1.music.126.net/zM4G5cgsfQxsgDftD4RJIw==/109951169299510813.jpg",
+        }
+    ]
+})
