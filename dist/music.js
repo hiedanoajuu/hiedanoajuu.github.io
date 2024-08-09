fd88b7c519f78f13a6f3ebddd63367367d98df3a
capetdevalois <2703998852@qq.com>
2024-08-09 14:24:33 +0800

    Site updated: 2024-08-09 14:24:24

--- dist/music.js
+++ dist/music.js
@@ -7,9 +7,9 @@ const ap = new APlayer({
     lrcType: 0,
     audio: [
         {
-            name: "Witching Dream",
+            name: "Selene's light",
             artist: "上海アリス幻樂団",
-            url: "http://music.163.com/song/media/outer/url?id=510030.mp3",
+            url: "http://music.163.com/song/media/outer/url?id=510035.mp3",
             cover:
                 "https://p1.music.126.net/zM4G5cgsfQxsgDftD4RJIw==/109951169299510813.jpg",
         }
